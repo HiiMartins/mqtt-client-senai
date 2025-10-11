cba65e5bbe7df81c1f64f646a1ee909bb1b900c9
HiiMartins <higor.dsm10@gmail.com>
2025-10-11 19:11:53 -0300

    alterando a conexao para o broker local

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,9 @@
 const mqtt = require("mqtt");
-const client = mqtt.connect("mqtt://test.mosquitto.org");
+const client = mqtt.connect("mqtt://127.0.0.1:1883");
 
+// Abre a conexão com o broker
 client.on("connect", () => {
+  // Se inscreve e publica no tópico presence
   client.subscribe("presence", (err) => {
     if (!err) {
       client.publish("presence", "Hello mqtt");
@@ -9,8 +11,8 @@ client.on("connect", () => {
   });
 });
 
+//Recebe as mensagens dos tópicos que o cliente é incristo
 client.on("message", (topic, message) => {
-  // Sem client.end() - ficaria executando para sempre
   console.log("Mensagem recebida:", message.toString());
-  // O script não termina, continua ouvindo novas mensagens
+  // Sem client.end() - a função não é finalizada. 
 });
